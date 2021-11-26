94e6de06a591b7d04059d481ae89c763c0d9e89c
Ahmed Chouaya <chouaya.ahmed83@gmail.com>
2021-11-27 00:35:28 +0100

    commit on 'main'

---
+++ src/component/profile/Address.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+function Address () {
+    return <h1>My email is: chouaya.ahmed83@gmail.com</h1>;
+}
+export default Address
---
+++ src/component/profile/FullName.js
@@ -0,0 +1,7 @@
+import React from "react"; 
+
+
+function FullName() {
+    return <h1>My name is: Ahmed Chouaya</h1>;
+}
+export default FullName
